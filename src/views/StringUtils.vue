eb8fe530292409a71e6db2e5fda02551b38fc273
Chuan Jin <chuan.jin.813@gmail.com>
2019-03-12 22:21:05 +0100

    Add string case convert tool

---
+++ src/views/StringUtils.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="string_utils">
+        <h1>This is an String Utilities</h1>
+
+        <div>
+
+            <textarea rows="10" cols="100" v-model="input_strings">
+            </textarea>
+            <button v-on:click="reverse">Reverse</button>
+            <button v-on:click="upprer_case">Upper Case</button>
+            <button v-on:click="lower_case">Lower Case</button>
+            <button v-on:click="sentense_case">Sentense Case</button>
+            <button v-on:click="proper_case">Proper Case</button>
+        </div>
+
+
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            input_strings: "Paste or Type your string here."
+        }
+    },
+
+    methods: {
+        reverse: function () {
+            this.input_strings = this.input_strings.split('').reverse().join('')
+        },
+
+        upprer_case: function () {
+            this.input_strings = this.input_strings.toUpperCase()
+        },
+
+        lower_case: function () {
+            this.input_strings = this.input_strings.toLowerCase()
+        },
+
+        capitalize: function(s) {
+            return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()
+        },
+
+        sentense_case: function() {
+            this.input_strings = this.capitalize(this.input_strings)
+        },
+
+        proper_case: function () {
+            this.input_strings =
+                this.input_strings.split(' ').map(function(s) {
+                    return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()
+                }).join(' ')
+        }
+    }
+}
+
+</script>
