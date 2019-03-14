abb48f7870e87898511d405f07a330b448c08cb4
Chuan Jin <chuan.jin.813@gmail.com>
2019-03-14 21:43:56 +0100

    Add footer and remove nav

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/string">String</router-link> |
-      <router-link to="/json">json</router-link>
-    </div>
+    <!-- <div id="nav"> -->
+    <!--   <router-link to="/">Home</router-link> | -->
+    <!--   <router-link to="/string">String</router-link> | -->
+    <!--   <router-link to="/json">Json</router-link> -->
+    <!-- </div> -->
     <router-view/>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,36 @@
 <template>
-    <section class="hero is-info is-large is-bold">
-        <div class="hero-body">
-            <div class="container">
-                <h1 class="title">
-                    This online tools contain a few handy utilities
-                </h1>
-                <h2 class="subtitle">
-                    String Case Converter, Pretty/Minify Json
-                </h2>
+    <div>
+
+        <section class="hero is-info is-large is-bold">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        This online tools contain a few handy utilities
+                    </h1>
+                    <h2 class="subtitle">
+                        <router-link to="/string"><u>String Case Converter</u></router-link> | 
+                        <router-link to="/json"><u>Pretty or Minify JSON</u></router-link>
+                    </h2>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
+
+        <section>
+            <hr>
+            <div class="container">
+                <div class="content has-text-centered">
+                    <p>
+                        Created by <a href="https://github.com/chuanjin">Chuan Jin</a>. The source code is licensed
+                        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+                        <p>
+                            <a class="icon" href="https://github.com/chuanjin/handy">
+                                <i class="fab fa-github"></i>
+                            </a>
+                        </p>
+                </div>
+            </div>
+        </section>
+
+    </div>
 
 </template>
